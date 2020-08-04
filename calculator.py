eee129142c4ffb66039a27451196cdb3dd77c2c4
cianbrophy <cian.brophy@ucdconnect.ie>
2020-08-04 15:19:27 +0100

    window size corrected

--- calculator.py
+++ calculator.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
 
     window.title("Cian's Calculator™")
 
-    window.geometry("800x600")
+    window.geometry("380x450")
 
     equation = StringVar()
 
@@ -51,71 +51,71 @@ if __name__ == "__main__":
     equation.set('enter your value')
 
     button1 = Button(window, text=' 1 ', fg='black', bg='red', 
-                     command=lambda: press(1), height=10, width=10) 
+                     command=lambda: press(1), height=5, width=10) 
     button1.grid(row=2, column=0) 
   
     button2 = Button(window, text=' 2 ', fg='black', bg='red', 
-                     command=lambda: press(2), height=10, width=10) 
+                     command=lambda: press(2), height=5, width=10) 
     button2.grid(row=2, column=1) 
   
     button3 = Button(window, text=' 3 ', fg='black', bg='red', 
-                     command=lambda: press(3), height=10, width=10) 
+                     command=lambda: press(3), height=5, width=10) 
     button3.grid(row=2, column=2) 
   
     button4 = Button(window, text=' 4 ', fg='black', bg='red', 
-                     command=lambda: press(4), height=10, width=10) 
+                     command=lambda: press(4), height=5, width=10) 
     button4.grid(row=3, column=0) 
   
     button5 = Button(window, text=' 5 ', fg='black', bg='red', 
-                     command=lambda: press(5), height=10, width=10) 
+                     command=lambda: press(5), height=5, width=10) 
     button5.grid(row=3, column=1) 
   
     button6 = Button(window, text=' 6 ', fg='black', bg='red', 
-                     command=lambda: press(6), height=10, width=10) 
+                     command=lambda: press(6), height=5, width=10) 
     button6.grid(row=3, column=2) 
   
     button7 = Button(window, text=' 7 ', fg='black', bg='red', 
-                     command=lambda: press(7), height=10, width=10) 
+                     command=lambda: press(7), height=5, width=10) 
     button7.grid(row=4, column=0) 
   
     button8 = Button(window, text=' 8 ', fg='black', bg='red', 
-                     command=lambda: press(8), height=10, width=10) 
+                     command=lambda: press(8), height=5, width=10) 
     button8.grid(row=4, column=1) 
   
     button9 = Button(window, text=' 9 ', fg='black', bg='red', 
-                     command=lambda: press(9), height=10, width=10) 
+                     command=lambda: press(9), height=5, width=10) 
     button9.grid(row=4, column=2) 
   
     button0 = Button(window, text=' 0 ', fg='black', bg='red', 
-                     command=lambda: press(0), height=10, width=10) 
+                     command=lambda: press(0), height=5, width=10) 
     button0.grid(row=5, column=0) 
   
     plus = Button(window, text=' + ', fg='black', bg='red', 
-                  command=lambda: press("+"), height=10, width=10) 
+                  command=lambda: press("+"), height=5, width=10) 
     plus.grid(row=2, column=3) 
   
     minus = Button(window, text=' - ', fg='black', bg='red', 
-                   command=lambda: press("-"), height=10, width=10) 
+                   command=lambda: press("-"), height=5, width=10) 
     minus.grid(row=3, column=3) 
   
-    multiply = Button(window, text=' x ', fg='black', bg='red', 
-                      command=lambda: press("x"), height=10, width=10) 
+    multiply = Button(window, text=' × ', fg='black', bg='red', 
+                      command=lambda: press("x"), height=5, width=10) 
     multiply.grid(row=4, column=3) 
   
-    divide = Button(window, text=' / ', fg='black', bg='red', 
-                    command=lambda: press("/"), height=10, width=10) 
+    divide = Button(window, text=' ÷ ', fg='black', bg='red', 
+                    command=lambda: press("/"), height=5, width=10) 
     divide.grid(row=5, column=3) 
   
     equal = Button(window, text=' = ', fg='black', bg='red', 
-                   command=equalpress, height=10, width=10) 
+                   command=equalpress, height=5, width=10) 
     equal.grid(row=5, column=2) 
   
-    clear = Button(window, text='Clear', fg='black', bg='red', 
-                   command=clear, height=10, width=10) 
+    clear = Button(window, text='CLR', fg='black', bg='red', 
+                   command=clear, height=5, width=10) 
     clear.grid(row=5, column='1') 
   
     Decimal= Button(window, text='.', fg='black', bg='red', 
-                    command=lambda: press('.'), height=10, width=10) 
+                    command=lambda: press('.'), height=5, width=10) 
     Decimal.grid(row=6, column=0) 
     
     window.mainloop() 
